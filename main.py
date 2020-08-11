2846bf7a374314956229a7da2da1fef323ff8ac2
Shiv_10 <shivanshsharma2012@gmail.com>
2020-08-11 20:44:44 +0530

    feat: added blinking detection

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 from cv2 import cv2
 import numpy as np
 import dlib
+from math import hypot
+
 
 def midpoint(p1, p2):
     return  (p1.x+p2.x)//2, (p1.y+p2.y)//2
@@ -33,6 +35,16 @@ while True:
 
         hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), thickness=1)
         ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), thickness=1)
+
+        hor_line_len = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+        ver_line_len = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+        ratio = hor_line_len/ver_line_len
+
+        if ratio > 5:
+            print("Blinked")
+
+        
 
 
     
