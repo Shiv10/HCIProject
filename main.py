0a4a1c2d7cf808b4fc5904d9d13eabf4279d6901
Shiv_10 <shivanshsharma2012@gmail.com>
2020-09-12 14:55:44 +0530

    fix: code refactor

--- main.py
+++ main.py
@@ -11,6 +11,25 @@ cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
 
+def get_blinking_ratio(eye_points, facial_landmarks):
+        left_point = (facial_landmarks.part(eye_points[0]).x , facial_landmarks.part(eye_points[0]).y)
+        right_point = (facial_landmarks.part(eye_points[3]).x , facial_landmarks.part(eye_points[3]).y)
+
+        center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+        center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+
+
+        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), thickness=1)
+        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), thickness=1)
+
+        hor_line_len = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
+        ver_line_len = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
+
+        ratio = hor_line_len/ver_line_len
+
+
+        return ratio
+
 
 while True:
     _, frame = cap.read()
@@ -26,22 +45,11 @@ while True:
         # cv2.rectangle(frame, (x ,y), (x1, y1), (255, 0, 0))
 
         landmarks = predictor(gray, face)
-        left_point = (landmarks.part(36).x , landmarks.part(36).y)
-        right_point = (landmarks.part(39).x , landmarks.part(39).y)
 
-        center_top = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom = midpoint(landmarks.part(41), landmarks.part(40))
+        left_eye_ratio = get_blinking_ratio([36,37,38,39,40,41], landmarks)
 
 
-        hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), thickness=1)
-        ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), thickness=1)
-
-        hor_line_len = hypot((left_point[0] - right_point[0]), (left_point[1] - right_point[1]))
-        ver_line_len = hypot((center_top[0] - center_bottom[0]), (center_top[1] - center_bottom[1]))
-
-        ratio = hor_line_len/ver_line_len
-
-        if ratio > 5:
+        if left_eye_ratio > 5.39:
             print("Blinked")
 
         
