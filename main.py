a165b6e782776a6dd011db7a1bac183dafa43677
Navneet Melarkode <navneet.melarkode@gmail.com>
2020-10-14 11:14:24 +0530

    feat: Seperated Eye

--- main.py
+++ main.py
@@ -12,7 +12,19 @@ font = cv2.FONT_HERSHEY_SIMPLEX
 cap = cv2.VideoCapture(0)
 detector = dlib.get_frontal_face_detector()
 predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")
-blinkcount = 0
+
+def get_blinking_ratio(eye_points, facial_landmarks):
+    left_point = (facial_landmarks.part(eye_points[0]).x , facial_landmarks.part(eye_points[0]).y)
+    right_point = (facial_landmarks.part(eye_points[3]).x , facial_landmarks.part(eye_points[3]).y)
+    center_top = midpoint(facial_landmarks.part(eye_points[1]), facial_landmarks.part(eye_points[2]))
+    center_bottom = midpoint(facial_landmarks.part(eye_points[5]), facial_landmarks.part(eye_points[4]))
+    #hor_line = cv2.line(frame, left_point, right_point, (0, 255, 0), thickness=1)
+    #ver_line = cv2.line(frame, center_top, center_bottom, (0, 255, 0), thickness=1)
+    hor_line_len = hypot((left_point[0] - right_point[0]),(left_point[1] - right_point[1]))
+    ver_line_len = hypot((center_top[0] - center_bottom[0]),(center_top[1]-center_bottom[1]))
+
+    ratio = hor_line_len/ver_line_len
+    return ratio
 
 while True:
     _, frame = cap.read()
@@ -28,32 +40,37 @@ while True:
         # cv2.rectangle(frame, (x ,y), (x1, y1), (255, 0, 0))
 
         landmarks = predictor(gray, face)
-        left_point_1 = (landmarks.part(36).x , landmarks.part(36).y)
-        right_point_1 = (landmarks.part(39).x , landmarks.part(39).y)
-        left_point_2 = (landmarks.part(42).x , landmarks.part(42).y)
-        right_point_2 = (landmarks.part(45).x , landmarks.part(45).y)
+        
+        left_eye_ratio = get_blinking_ratio([36,37,38,39,40,41], landmarks)
+        right_eye_ratio = get_blinking_ratio([42,43,44,45,46,47],landmarks)
 
-        center_top_1 = midpoint(landmarks.part(37), landmarks.part(38))
-        center_bottom_1 = midpoint(landmarks.part(41), landmarks.part(40))
-        center_top_2 = midpoint(landmarks.part(43), landmarks.part(44))
-        center_bottom_2 = midpoint(landmarks.part(47), landmarks.part(46))
+        blinking_ratio = (left_eye_ratio+right_eye_ratio)/2
+        if blinking_ratio>5:
+            cv2.putText(frame, 'BLINKING',(50,150),font,7,(255,0,0))
+        
+        left_eye_region = np.array([(landmarks.part(36).x, landmarks.part(36).y),
+                                    (landmarks.part(37).x, landmarks.part(37).y),
+                                    (landmarks.part(38).x, landmarks.part(38).y),
+                                    (landmarks.part(39).x, landmarks.part(39).y),
+                                    (landmarks.part(40).x, landmarks.part(40).y),
+                                    (landmarks.part(41).x, landmarks.part(41).y)], np.int32)
+        cv2.polylines(frame,[left_eye_region],True,(0,0,255),2)
 
-        hor_line_1 = cv2.line(frame, left_point_1, right_point_1, (0, 255, 0), thickness=1)
-        ver_line_1 = cv2.line(frame, center_top_1, center_bottom_1, (0, 255, 0), thickness=1)
-        hor_line_2 = cv2.line(frame, left_point_2, right_point_2, (0, 255, 0), thickness=1)
-        ver_line_2 = cv2.line(frame, center_top_2, center_bottom_2, (0, 255, 0), thickness=1)
+        
 
-        hor_line_1_len = hypot((left_point_1[0] - right_point_1[0]),(left_point_1[1] - right_point_1[1]))
-        ver_line_1_len = hypot((center_top_1[0] - center_bottom_1[0]),(center_top_1[1]-center_bottom_1[1]))
+        min_x = np.min(left_eye_region[:,0])
+        max_x = np.max(left_eye_region[:,0])
+        min_y = np.min(left_eye_region[:,1])
+        max_y = np.max(left_eye_region[:,1])
 
-        hor_line_2_len = hypot((left_point_2[0] - right_point_2[0]),(left_point_2[1] - right_point_2[1]))
-        ver_line_2_len = hypot((center_top_2[0] - center_bottom_2[0]),(center_top_2[1]-center_bottom_2[1]))
+        eye = frame[min_y:max_y, min_x:max_x]
+        gray_eye = cv2.cvtColor(eye,cv2.COLOR_BGR2GRAY)
+        _,threshold_eye = cv2.threshold(gray_eye, 70, 255, cv2.THRESH_BINARY)
 
-        ratio_1 = hor_line_1_len/ver_line_1_len
-        ratio_2 = hor_line_2_len/ver_line_2_len
-
-        if ratio_1 >5 and ratio_2>5:
-            cv2.putText(frame, "BLINKING",(50, 150),font, 3, (255,0,0))
+        threshold_eye = cv2.resize(threshold_eye, None, fx=5,fy=5)
+        eye = cv2.resize(eye,None, fx=5, fy=5)
+        cv2.imshow("Eye",eye)
+        cv2.imshow("Threshold",threshold_eye)
 
     cv2.imshow('Frame', frame)
     key = cv2.waitKey(1)
